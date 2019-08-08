a683f03c2d13215225ed2e79ccc7108ff2b2ee2b
Oleksandr Nemyrovskyi <AlexNem@users.noreply.github.com>
2019-08-08 19:55:41 +0300

    add DataPicker

--- app/src/main/java/com/example/exchangeconverter/ui/activity/MainActivity.kt
+++ app/src/main/java/com/example/exchangeconverter/ui/activity/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.exchangeconverter.ui.activity
 
+import android.app.DatePickerDialog
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.exchangeconverter.R
@@ -9,10 +11,13 @@ import com.example.exchangeconverter.ui.fragment.first.MyFirstRecycler
 import com.example.exchangeconverter.ui.fragment.second.MySecondRecycler
 import kotlinx.android.synthetic.main.fragment_all_currency.*
 import kotlinx.android.synthetic.main.fragment_currency.*
+import java.util.*
 
 class MainActivity : AppCompatActivity()
 {
     var mockData = MockCurrencyData()
+    val calendar: Calendar = Calendar.getInstance()
+
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -23,6 +28,27 @@ class MainActivity : AppCompatActivity()
 
         setFirstRecycler()
         setSecondRecycler()
+        setDataPickers()
+
+
+    }
+
+    fun setDataPickers(){
+        iv_first_calendar.setOnClickListener(View.OnClickListener { initDataPicker() })
+        iv_second_calendar.setOnClickListener(View.OnClickListener { initDataPicker() })
+    }
+    fun initDataPicker(){
+        val d = DatePickerDialog.OnDateSetListener { view, year, month, dayOfMonth ->
+            calendar.get(Calendar.YEAR)
+            calendar.get(Calendar.MONTH)
+            calendar.get(Calendar.DAY_OF_MONTH)
+        }
+        val picker = DatePickerDialog(this,
+                d,
+                calendar.get(Calendar.YEAR),
+                calendar.get(Calendar.MONTH),
+                calendar.get(Calendar.DAY_OF_MONTH))
+        picker.show()
     }
 
 //    fun getDB(): List<CurrencyEntity>{
